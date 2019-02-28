1826b8b5e44c6dcb83e5bfb8b27d02ce1d43b68b
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2019-02-28 17:11:52 +0100

    first commit

---
+++ R/help_path.R
@@ -0,0 +1,58 @@
+#stripped down version of utils::help
+#returns the help path only
+help_path <-
+  function (topic,
+            package = NULL,
+            lib.loc = NULL,
+            try.all.packages = getOption("help.try.all.packages")){
+
+    if (!missing(package))
+      if (is.name(y <- substitute(package)))
+        package <- as.character(y)
+
+      ischar <- tryCatch(
+        is.character(topic) && length(topic) ==
+          1L,
+        error = function(e)
+          FALSE
+      )
+      if (!ischar) {
+        reserved <- c(
+          "TRUE",
+          "FALSE",
+          "NULL",
+          "Inf",
+          "NaN",
+          "NA",
+          "NA_integer_",
+          "NA_real_",
+          "NA_complex_",
+          "NA_character_"
+        )
+        stopic <- deparse(substitute(topic))
+        if (!is.name(substitute(topic)) && !stopic %in% reserved)
+          stop("'topic' should be a name, length-one character vector or reserved word")
+        topic <- stopic
+      }
+      paths <- utils:::index.search(topic,
+                                    find.package(if (is.null(package))
+                                      loadedNamespaces()
+                                      else
+                                        package, lib.loc, verbose = FALSE))
+      try.all.packages <-
+        !length(paths) && is.logical(try.all.packages) &&
+        !is.na(try.all.packages) &&
+        try.all.packages && is.null(package) &&
+        is.null(lib.loc)
+      if (try.all.packages) {
+        for (lib in .libPaths()) {
+          packages <- .packages(TRUE, lib)
+          packages <- packages[is.na(match(packages, .packages()))]
+          paths <- c(paths, index.search(topic, file.path(lib,
+                                                          packages)))
+        }
+        paths <- paths[nzchar(paths)]
+      }
+      paths
+  }
+
---
+++ R/rdoc.R
@@ -0,0 +1,131 @@
+#' Pretty print R docs
+#'
+#' Refer to R docs in the console
+#'
+#'
+#' @export
+rd <- function(topic,
+               by_section = TRUE,
+               options = rdoc_options(),
+               package = NULL,
+               lib.loc = NULL) {
+  rd_ <- Rdoc$new(topic, by_section, options, package, lib.loc)
+  rd_$show()
+}
+
+rd_example <- function(topic, options = rdoc_options()) {
+  rd <- Rdoc(topic, by_section, options)
+  rd$show("examples")
+}
+
+
+#' @importFrom R6 R6Class
+Rdoc <- R6Class(
+  "Rdoc",
+  public = list(
+    topic = NULL,
+    path = NULL,
+    pkg = NULL,
+    rd_file = NULL,
+    opts = NULL,
+    initialize = function(topic,
+                          by_section,
+                          options,
+                          package,
+                          lib.loc) {
+      self$topic <- private$topic_(topic)
+      self$path <- help_path(self$topic) ### add in params..
+      self$pkg <- basename(dirname(dirname(self$path)))
+      self$opts <- options
+      self$rd_file <- private$tmp_rd_(self$path)
+      private$by_section = by_section
+      private$rd_txt <- readLines(self$rd_file)
+    },
+    show = function(which = NULL){
+      s <- private$list_sections(private$rd_txt)
+
+      if (!is.null(which))
+        return(private$out_(s[[which]]))
+      if (!private$by_section || !interactive())
+        return(private$out_(s))
+
+      private$n_sections <- length(s)
+      private$current_section <- 2L
+      private$out_(s[1L:2L])
+      while(private$current_section != private$n_sections){
+        p <- readline("")
+        if (substr(p, 1L, 1L) == "q")
+          break
+        private$current_section <- private$current_section + 1L
+        private$out_(s[private$current_section])
+      }
+    }
+  ),
+  private = list(
+    rd_txt = NULL,
+    by_section = TRUE,
+    n_sections = 0L,
+    current_section = 0L,
+    topic_ = function(tp){
+      if (is.character(tp))
+        return(tp)
+      deparse(substitute(tp))
+    },
+    tmp_rd_ = function(file){
+      rd_file <- tempfile(fileext = ".rd")
+      tools::Rd2txt(
+        utils:::.getHelpFile(file),
+        out = rd_file,
+        options = list(
+          underline_titles = TRUE,
+          width = getOption('width'),
+          code_quote = TRUE,
+          item_bullet = self$opts$item_bullet
+        )) ##fix - assign
+      rd_file
+    },
+    list_sections = function(o) {
+      headers <- which(grepl("^_\b", o))
+      #on error?
+      nm <- character(length(headers))
+      sections <- lapply(seq_along(headers), function(i) {
+        h <- headers[i]
+        nm[i] <<- tolower(gsub("_\b|:", "", o[h]))
+        o[h] <<- self$opts$section(o[h])
+        if (i == length(headers))
+          return(o[headers[i]:length(o)])
+        o[h:(headers[i + 1] - 1L)]
+      })
+      names(sections) <- nm
+      sections
+    },
+    out_ = function(s) cat(paste(private$append_(s), collapse = "\n")),
+    append_ = function(l) Reduce(append, l)
+  )
+)
+
+
+
+#' R doc options
+#'
+#' Set output styles
+#'
+#' @import crayon
+#' @import prettycode
+#' @import cli
+#' @export
+rdoc_options <- function(pkg_header = cli::rule,
+                         title = crayon::bold,
+                         section_titles = crayon::underline,
+                         arg_params = crayon::underline,
+                         code = prettycode::highlight,
+                         item_bullet = cli::symbol$bullet) {
+  list(
+    pkg_header = pkg_header,
+    title = title,
+    section_titles = section_titles,
+    arg_params = arg_params,
+    code = code,
+    item_bullet = item_bullet
+  )
+}
