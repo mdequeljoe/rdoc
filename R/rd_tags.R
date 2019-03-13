c3482613171fedba2b3db1141db4dba4eec47761
mdequeljoe <matthew.dequeljoe@gmail.com>
2019-03-13 15:15:34 +0100

    add inline code formatting

--- R/rd_tags.R
+++ R/rd_tags.R
@@ -6,6 +6,20 @@ tag_ <- function(l, default = character(1)) {
   x
 }
 
+get_subtags <- function(l){
+
+  subtag_ <- function(l){
+    lapply(l, function(d){
+      if (is.list(d))
+        subtag_(d)
+      else
+        tag_(d)
+    })
+  }
+  l <- subtag_(l)
+  unlist(l)
+}
+
 apply_fmt <- function(l, op, cl){
   lapply(l, function(d){
     if (is.list(d)){
@@ -30,13 +44,33 @@ convert_tag <- function(op, cl){
   }
 }
 
+
 fmt_italics <- convert_tag("\033[3m", "\033[23m")
 fmt_bold <- convert_tag("\033[1m", "\033[22m")
 fmt_squotes <- convert_tag("'", "'")
 
+fmt_inline_code <- function(l){
+  st <- get_subtags(l)
+
+  if (!all(st == "RCODE"))
+    return(l)
+
+  l <- unlist(l)
+  l <- tryCatch(
+    prettycode::highlight(l),
+    error = function(e) l,
+    warning = function(w) l
+  )
+  attr(l, "Rd_tag") <- "TEXT"
+  l
+}
+
 format_rdo <- function(l) {
 
   att <- attributes(l)
+  if (tag_(l) %in% c("\\example", "\\examples", "\\usage"))
+    return(l)
+
   o <- lapply(l, function(d) {
     if (is.list(d))
       d <- format_rdo(d)
@@ -50,9 +84,9 @@ format_rdo <- function(l) {
     attr(x, "Rd_tag") <- "TEXT"
     return(x)
   }
-#
-#   if (tag_(o) == "\\sQuote")
-#     return(fmt_squotes(o))
+
+  if (tag_(o) == "\\code")
+    return(fmt_inline_code(o))
 
   if (tag_(o) == "\\emph")
     return(fmt_italics(o))
