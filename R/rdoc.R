c3386d72a582a534e8b9429b8f8a54703e16f418
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2019-03-17 12:24:59 +0100

    add docs

--- R/rdoc.R
+++ R/rdoc.R
@@ -22,32 +22,46 @@ rd_ <- function(which = NULL){
 #' R docs
 #'
 #' Refer to colourised R docs in the console
+#' @family rd access
 #' @aliases rd
-#' @param topic character(1), help topic
-#' @param by_section logical(1), should docs be shown section by section or all
+#' @param topic \code{character(1)}, help topic
+#' @param by_section \code{logical(1)}, should docs be shown section by section or all
 #'   at once? Defaults to TRUE.
-#' @param package character(1), package of help topic. Defaults to NULL.
-#' @param lib.loc character(1), library location of package. Defaults to null.
+#' @param package \code{character(1)}, package of help topic. Defaults to NULL.
+#' @param lib.loc \code{character(1)}, library location of package. Defaults to null.
 #'
 #' @examples \dontrun{
 #'
-#' rd("min", by_section = FALSE)
+#' rd("rd", by_section = FALSE)
+#' rd_example("min")
 #'
 #' }
 #' @export
 rd <- rd_()
 
+#' @family rd access
+#' @inherit rd
 #' @export
 rd_details <- rd_("details")
 
+#' @inheritParams rd
+#' @aliases rd_examples
 #' @export
 rd_example <- rd_("examples")
+
+#' @export
+rd_examples <- rd_example
 
 
 #' R doc options
 #'
-#' Set output styles
-#'
+#' Set output formatting styles
+#' @param pkg_header package header information. Defaults to \code{cli::rule}
+#' @param title topic title. Defaults to \code{crayon::bold}
+#' @param section_titles section titles Defaults to \code{crayon::underline}
+#' @param arg_params argument paramets. Defaults to \code{crayon::underline}
+#' @param code_style code sections and inline code. Defaults to \code{prettycode::default_style()}
+#' @param item_bullet item bullet points. Defaults to \code{cli::symbol$bullet}
 #' @import crayon
 #' @import prettycode
 #' @import cli
