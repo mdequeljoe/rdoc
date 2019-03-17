167b74f1783568808403cc047da6b7d4be883ed4
Matthew de Queljoe <matthew.dequeljoe@gmail.com>
2019-03-17 11:59:23 +0100

    add basic rd text output tests

--- tests/testthat/test_rd.R
+++ tests/testthat/test_rd.R
@@ -1,9 +1,26 @@
 context("rd")
 
-# test_that("individual sections returned", {
-#   k <- capture.output(rd_example())
-# })
+cap_ <- capture.output
 
-# test_that("rd files also accepted", {
-#
-#})
+test_that("rd produces text output", {
+  test_ <- function(x){
+    expect_true(length(x) > 0L)
+    expect_true(is.character(x))
+  }
+  x <- cap_(rd("min", by_section = FALSE))
+  test_(x)
+
+  xd <- cap_(rd_details("min", by_section = FALSE))
+  test_(xd)
+  expect_true(length(xd) < length(x))
+
+  xe <- cap_(rd_example("min", by_section = FALSE))
+  test_(xe)
+  expect_true(length(xe) < length(x))
+
+  x <- cap_(rd(min, by_section = FALSE))
+  test_(x)
+
+  x <- cap_(rd(min, by_section = FALSE, package = base))
+  test_(x)
+})
